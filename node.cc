d87a552f913afbadde9581e5b38c60490d8ae9d5
shineinjar <42262657+shineinjar@users.noreply.github.com>
2018-08-14 13:08:27 +0900

    fix indentation

--- node.cc
+++ node.cc
@@ -6,47 +6,47 @@ Node::Node(char data) : m_next(nullptr), m_prev(nullptr), m_data(data) {
 }
 
 char Node::GetData() {
-	return m_data;
+    return m_data;
 }
 
 Node* Node::GetPreviousNode() {
-	return m_prev;
+    return m_prev;
 }
 
 Node* Node::GetNextNode() {
-	return m_next;
+    return m_next;
 }
 
 Node* Node::InsertPreviousNode(char data) {
-	Node* node = new Node(data);
-	node->m_prev = m_prev;
-	node->m_next = this;
-	if (m_prev)
-	{
-		m_prev->m_next = node;
-	}
-	m_prev = node;
-	return node;
+    Node* node = new Node(data);
+    node->m_prev = m_prev;
+    node->m_next = this;
+    if (m_prev)
+    {
+        m_prev->m_next = node;
+    }
+    m_prev = node;
+    return node;
 }
 
 Node* Node::InsertNextNode(char data) {
-	Node* node = new Node(data);
-	node->m_prev = this;
-	node->m_next = m_next;
-	if (m_next)
-	{
-		m_next->m_prev = node;
-	}
-	m_next = node;
-	return node;
+    Node* node = new Node(data);
+    node->m_prev = this;
+    node->m_next = m_next;
+    if (m_next)
+    {
+        m_next->m_prev = node;
+    }
+    m_next = node;
+    return node;
 }
 
 bool Node::ErasePreviousNode() {
-	if (!m_prev)
+    if (!m_prev)
     {
         return false;
     }
-	EraseNode(m_prev);
+    EraseNode(m_prev);
     return true;
 }
 
@@ -55,14 +55,14 @@ bool Node::EraseNextNode() {
     {
         return false;
     }
-	EraseNode(m_next);
+    EraseNode(m_next);
     return true;
 }
 
 void Node::EraseNode(Node* node)
 {
-	Node* prev = node->m_prev;
-	Node* next = node->m_next;
+    Node* prev = node->m_prev;
+    Node* next = node->m_next;
     if (prev)
     {
         prev->m_next = next;
@@ -71,6 +71,6 @@ void Node::EraseNode(Node* node)
     {
         next->m_prev = prev;
     }
-	delete node;
+    delete node;
 }
 
--- node.h
+++ node.h
@@ -4,22 +4,22 @@
 #define NODE_H_
 
 class Node {
- public:
-  explicit Node(char data);
-  char GetData();
-  Node* InsertPreviousNode(char data);
-  Node* InsertNextNode(char data);
-  Node* GetPreviousNode();
-  Node* GetNextNode();
-  bool ErasePreviousNode();
-  bool EraseNextNode();
+public:
+    explicit Node(char data);
+    char GetData();
+    Node* InsertPreviousNode(char data);
+    Node* InsertNextNode(char data);
+    Node* GetPreviousNode();
+    Node* GetNextNode();
+    bool ErasePreviousNode();
+    bool EraseNextNode();
 
 private:
-	static void EraseNode(Node* node);
+    static void EraseNode(Node* node);
 
-	Node* m_next;
-	Node* m_prev;
-	char m_data;
+    Node* m_next;
+    Node* m_prev;
+    char m_data;
 };
 
 #endif  // NODE_H_
